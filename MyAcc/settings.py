dd038f8be644c1a98a688a9b1aff777042c4dc39
Claudio1108 <claudio.alicino@libero.it>
2019-11-19 23:43:20 +0100

    fix

--- MyAcc/settings.py
+++ MyAcc/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = '%c%tj3bv#%dpyp^w)bx3zfg3$6g=!xrgh0m(&tll3nj5f8sk!3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.43.60', '192.168.43.143']
+ALLOWED_HOSTS = ['127.0.0.1']
 #ALLOWED_HOSTS = ['calicino.pythonanywhere.com']
 
 # Application definition
